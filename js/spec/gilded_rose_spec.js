32aece5357f2dec98d87480ed0a0ef3aa300c7c1
Anna Beddow <anna.beddow@gmail.com>
2017-01-17 18:18:30 +0000

    added all tests for regular items for js

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,18 +1,30 @@
 describe("Gilded Rose", function(){
+
   describe("Regular Items", function(){
+
     beforeEach(function(){
-      items = [ new Item("Soup", 20, 20) ];
+      items = [ new Item("Soup", 10, 20), new Item("Bread", 10, 0), new Item("Milk", 0, 10) ];
     });
-  //example test
-   it("should check the name of a regular item", function() {
+    it("should check the name of a regular item", function() {
       update_quality();
       expect(items[0].name).toEqual("Soup");
     });
-
-    it("should reduce the quality of a regular item by one", function() {
-      items = [ new Item("foo", 0, 0) ];
+    it("should reduce the sell_in of a regular item by one", function() {
       update_quality();
-      expect(items[0].name).toEqual("foo");
-    });   
+      expect(items[0].sell_in).toEqual(9);
+    });
+    it("should reduce the quality of a regular item by one", function() {
+      update_quality();
+      expect(items[0].quality).toEqual(19);
+    });
+    it("should not let the quality of a regular item be negative", function(){
+      update_quality();
+      expect(items[1].quality).toEqual(0);
+    });
+    it("should reduce the quality of an item twice as fast one sell_in reaches zero", function(){
+      update_quality();
+      expect(items[2].quality).toEqual(8);
+    });
   });
+
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -8,18 +8,28 @@ var items = [];
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
+    //if not aged brie or backstage
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      //and the quality is greater than 0
       if (items[i].quality > 0) {
+        //and not sulfuras
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          //item quality should reduce by 1
           items[i].quality = items[i].quality - 1;
         }
       }
     } else {
+      // else if item's quality is less than 50
       if (items[i].quality < 50) {
+        // quality increases by 1
         items[i].quality = items[i].quality + 1;
+        //and if the item is back stage
         if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          // and there is less than 11 days to sell
           if (items[i].sell_in < 11) {
+            //and the quality is less than 50
             if (items[i].quality < 50) {
+              // the quality increases by 1
               items[i].quality = items[i].quality + 1;
             }
           }
